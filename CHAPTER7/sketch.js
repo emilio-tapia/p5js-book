cbd1217e952c43a7f410779abfe3bd7764f1f0f3
Emilio Tapia <emta.develop@gmail.com>
2022-04-19 13:07:31 -0500

    practice finished

--- CHAPTER7/sketch.js
+++ CHAPTER7/sketch.js
@@ -1,5 +1,5 @@
 const canvasWidth = window.innerWidth
-const canvasHeight = window.innerHeight * 2
+const canvasHeight = window.innerHeight * 3
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
@@ -18,27 +18,65 @@ function draw() {
   noStroke();
   let diameter = 50
   let x = canvasWidth/2;
-  let y = canvasHeight/4;
+  let y = canvasHeight/6;
   let gapX = canvasWidth/8;
-  let gapY = canvasHeight/16;
+  let gapY = canvasHeight/24;
   let randomCustom = (i, j) => Math.round((Math.random()*100) - (i-j+20))
 
   for(let i=0; i<10; i++){
     for(let j=0; j<10; j++){
-    fill(random(255) - 50, randomCustom(i, j) + 50); 
+    fill(
+      random(255) - 50, 
+      randomCustom(i, j) + 50); 
     // p5.js random ==> random(range value)
     // custom random ==> randomCustom(first value, second value)
-    ellipse(x * i/6 + gapX, y * j/6 + gapY, random(diameter), random(diameter))
+    ellipse(
+        x * i/6 + gapX, 
+        y * j/6 + gapY, 
+        random(diameter), 
+        random(diameter))
     }
   }
 
 
   for(let i=0; i<10; i++){
     for(let j=0; j<10; j++){
-    fill(random(255) - 50, randomCustom(i, j) + 50); 
-    ellipse(x * i/6 + gapX, y * j/6 + gapY + canvasHeight/2, noise(frameCount+i+j)*diameter, noise(frameCount+i+j)*diameter)
+    fill(
+      random(255) - 50, 
+      randomCustom(i, j) + 50); 
+    ellipse(
+      x * i/6 + gapX, 
+      y * j/6 + gapY + canvasHeight/3, 
+      noise(frameCount+i+j)*diameter, 
+      noise(frameCount+i+j)*diameter)
 
     }
+  }
+
+  let color = 0
+
+  for(let v=0; v<9; v++){
+
+    fill(
+      255 - v * 35, 
+      randomCustom(v, v) + 50); 
+    ellipse(
+      x * v/6 + gapX * 1.25, 
+      y + (canvasHeight/3 * 2), 
+      noise(frameCount+v)*diameter, 
+      noise(frameCount+v)*diameter)
+
+    color = color + v * 2
+
+      fill(
+        color + v * 10, 50); 
+    rect(
+        x * v/6 + gapX * 1.25, 
+        y + (canvasHeight/3 * 2), 
+        70, 
+       400)
+
+    // }
   }
 
 }
